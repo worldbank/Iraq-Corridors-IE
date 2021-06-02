692d386a37b9fd1c2a46f57c2353bebcb9f65f48
Chitra Balasubramanian <chitra_b@berkeley.edu>
2021-06-02 15:18:55 -0400

    market access reg

--- DataWork/Clusters/02_market_access_analysis/market_access_reg.R
+++ DataWork/Clusters/02_market_access_analysis/market_access_reg.R
@@ -89,7 +89,7 @@ cluster_df_20km <- cluster_df %>%
            MA_tt_rdlength_theta3_8_exclude20km_speed_limit_gs_before,
          diff_exclude50km = MA_tt_rdlength_theta3_8_exclude50km_speed_limit_gs_after - 
            MA_tt_rdlength_theta3_8_exclude50km_speed_limit_gs_before) #calculate differences
-
+#add log vars for difference
 
 # Summary stats -----------------------------------------------------------
 summary(cluster_df_20km$diff_exclude50km)
@@ -138,10 +138,10 @@ stargazer(reg5,
           header = F)
 
 #10km
-reg9 <- lm(diff_ntl~diff_noexclusion + factor(uid),data = cluster_df_20km)
-reg10 <- lm(diff_ntl~diff_exclude10km + factor(uid),data = cluster_df_20km)
-reg11 <- lm(diff_ntl~diff_exclude20km + factor(uid),data = cluster_df_20km)
-reg12 <- lm(diff_ntl~diff_exclude50km + factor(uid),data = cluster_df_20km)
+reg9 <- lm(diff_ntl~diff_noexclusion ,data = cluster_df_20km)
+reg10 <- lm(diff_ntl~diff_exclude10km ,data = cluster_df_20km)
+reg11 <- lm(diff_ntl~diff_exclude20km ,data = cluster_df_20km)
+reg12 <- lm(diff_ntl~diff_exclude50km,data = cluster_df_20km)
 
 stargazer(reg9,
           reg10,
@@ -153,6 +153,6 @@ stargazer(reg9,
           keep = c("diff_noexclusion","diff_exclude10km","diff_exclude20km","diff_exclude50km"),
           omit.stat = c("ser"),
           add.lines = list(c("Cluster FE", "Yes","Yes","Yes","Yes")),
-          out = file.path(data_file_path,"Clusters","Outputs","gs_20km.tex"),
+          #out = file.path(data_file_path,"Clusters","Outputs","gs_20km.tex"),
           float = F,
-          header = F)
+          header = F, type = "text")
